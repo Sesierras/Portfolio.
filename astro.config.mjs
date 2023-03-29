d4f0f280e3b1b3da3ff311abdf4a34d81e816776
sesierras <sesierras@unal.edu.co>
2023-03-29 15:27:18 -0500

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sesierras.github.io',
-  base: '/',
+  output: 'server',
+  adapter: vercel(),
+  // site: 'https://sesierras.github.io',
+  // base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
